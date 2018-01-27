fc6f69965540698a9b700ac4f537d661b472aed0
Muthanna A. Attyah <mkhuthir@gmail.com>
2018-01-27 21:31:24 +0400

    faster fk

--- src/FK_kr210.py
+++ src/FK_kr210.py
@@ -69,26 +69,26 @@ T6_7 = T6_7.subs(s)
 
 # Composition of Homogeneous Transforms
 # Transform from Base link to end effector (Gripper)
-T0_2 = simplify(T0_1 * T1_2) ## (Base) Link_0 to Link_2
-T0_3 = simplify(T0_2 * T2_3) ## (Base) Link_0 to Link_3
-T0_4 = simplify(T0_3 * T3_4) ## (Base) Link_0 to Link_4
-T0_5 = simplify(T0_4 * T4_5) ## (Base) Link_0 to Link_5
-T0_6 = simplify(T0_5 * T5_6) ## (Base) Link_0 to Link_6
-T0_7 = simplify(T0_6 * T6_7) ## (Base) Link_0 to Link_E (End Effector)
+T0_2 = (T0_1 * T1_2) ## (Base) Link_0 to Link_2
+T0_3 = (T0_2 * T2_3) ## (Base) Link_0 to Link_3
+T0_4 = (T0_3 * T3_4) ## (Base) Link_0 to Link_4
+T0_5 = (T0_4 * T4_5) ## (Base) Link_0 to Link_5
+T0_6 = (T0_5 * T5_6) ## (Base) Link_0 to Link_6
+T0_7 = (T0_6 * T6_7) ## (Base) Link_0 to Link_E (End Effector)
 
 # Correction Needed to Account for Orientation Difference Between
 # Difinition of Gripper Link_G in URDF versus DH Convention
 
 
-R_y = Matrix([[ cos(-np.pi/2),           0, sin(-np.pi/2), 0],
-              [             0,           1,             0, 0],
-              [-sin(-np.pi/2),           0, cos(-np.pi/2), 0],
-              [             0,           0,             0, 1]])
+R_y = Matrix([[ cos(-np.pi/2.),          0, sin(-np.pi/2.), 0 ],
+              [              0,         1.,              0, 0 ],
+              [-sin(-np.pi/2.),          0, cos(-np.pi/2.), 0 ],
+              [              0,          0,              0, 1 ]])
 
-R_z = Matrix([[    cos(np.pi), -sin(np.pi),             0, 0],
-              [    sin(np.pi),  cos(np.pi),             0, 0],
-              [             0,           0,             1, 0],
-              [             0,           0,             0, 1]])
+R_z = Matrix([[    cos(np.pi), -sin(np.pi),              0, 0 ],
+              [    sin(np.pi),  cos(np.pi),              0, 0 ],
+              [             0,           0,             1., 0 ],
+              [             0,           0,              0, 1.]])
 
 
 R_corr = simplify(R_z * R_y)
@@ -111,7 +111,7 @@ pprint(T0_6.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
 
 # Total Homogeneous Transform Between (Base) Link_0 and (End Effector) Link_E
 # With orientation correction applied
-T_total = simplify(T0_7 * R_corr)
+T_total = (T0_7 * R_corr)
 
 print("\nT_total Matrix : \n")
 pprint(T_total.evalf(subs={q1: 0, q2: 0, q3: 0, q4: 0, q5: 0, q6: 0}))
