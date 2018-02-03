48551f062930d6845efa97728ce10119abdd7a91
Muthanna A. Attyah <mkhuthir@gmail.com>
2018-02-03 15:29:49 +0400

    Update IK_server.py

--- src/kuka_arm/scripts/IK_server.py
+++ src/kuka_arm/scripts/IK_server.py
@@ -25,7 +25,6 @@ def handle_calculate_IK(req):
         print "No valid poses received"
         return -1
     else:
-        ### Your FK code here
         # Create symbols for DH param
         q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')                                 # joint angles theta
         d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')                                 # link offsets
@@ -34,15 +33,14 @@ def handle_calculate_IK(req):
         
         # Create Modified DH parameters
         dh = {alpha0:      0, a0:      0, d1:  0.75, q1:        q1,
-            alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2.+q2,
-            alpha2:      0, a2:   1.25, d3:     0, q3:        q3,
-            alpha3: -pi/2., a3: -0.054, d4:   1.5, q4:        q4,
-            alpha4:  pi/2., a4:      0, d5:     0, q5:        q5,
-            alpha5: -pi/2., a5:      0, d6:     0, q6:        q6,
-            alpha6:      0, a6:      0, d7: 0.303, q7:         0}
+              alpha1: -pi/2., a1:   0.35, d2:     0, q2: -pi/2.+q2,
+              alpha2:      0, a2:   1.25, d3:     0, q3:        q3,
+              alpha3: -pi/2., a3: -0.054, d4:   1.5, q4:        q4,
+              alpha4:  pi/2., a4:      0, d5:     0, q5:        q5,
+              alpha5: -pi/2., a5:      0, d6:     0, q6:        q6,
+              alpha6:      0, a6:      0, d7: 0.303, q7:         0}
         
         # Function to return homogeneous transform matrix
-
         def TF_Mat(alpha, a, d, q):
             TF = Matrix([[            cos(q),           -sin(q),           0,             a],
                         [ sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
@@ -60,25 +58,17 @@ def handle_calculate_IK(req):
         T6_7 = TF_Mat(alpha6, a6, d7, q7).subs(dh)
 
         # Composition of Homogeneous Transforms
-        # Transform from Base link to end effector (Gripper)
-        T0_2 = (T0_1 * T1_2) ## (Base) Link_0 to Link_2
-        T0_3 = (T0_2 * T2_3) ## (Base) Link_0 to Link_3
-        T0_4 = (T0_3 * T3_4) ## (Base) Link_0 to Link_4
-        T0_5 = (T0_4 * T4_5) ## (Base) Link_0 to Link_5
-        T0_6 = (T0_5 * T5_6) ## (Base) Link_0 to Link_6
-        T0_7 = (T0_6 * T6_7) ## (Base) Link_0 to Link_7 (End Effector)
+        # Transform from Base link_0 to end effector (Gripper) Link_7
+        T0_7 = (T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_7) 
 
         # Correction Needed to Account for Orientation Difference Between
-        # Definition of Gripper Link_G in URDF versus DH Convention
-
+        # Definition of Gripper Link_G in URDF versus DH Convention.
+        # Matrix is pre-calculated to improve performance.
         R_corr = Matrix([[0,0,1.0,0],[0,-1.0,0,0],[1.0,0,0,0],[0,0,0,1.0]])
 
         # Total Homogeneous Transform Between (Base) Link_0 and (End Effector) Link_7
         # With orientation correction applied
-
         T_total = (T0_7 * R_corr)
-
-        ###
 
         # Initialize service response
         joint_trajectory_list = []
