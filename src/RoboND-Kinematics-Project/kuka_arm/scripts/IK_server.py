0a97495dd4c8273aea2707a03427652760e376c8
Muthanna A. Attyah <mkhuthir@gmail.com>
2018-02-04 06:32:04 +0400

    Update IK_server.py

--- src/RoboND-Kinematics-Project/kuka_arm/scripts/IK_server.py
+++ src/RoboND-Kinematics-Project/kuka_arm/scripts/IK_server.py
@@ -37,6 +37,7 @@ def handle_calculate_IK(req):
               alpha5: -pi/2., a5:      0, d6:     0, q6:        q6,
               alpha6:      0, a6:      0, d7: 0.303, q7:         0}
         
+        # Define Modified DH Transformation matrix
         # Function to return homogeneous transform matrix
         def TF_Mat(alpha, a, d, q):
             TF = Matrix([[            cos(q),           -sin(q),           0,             a],
@@ -44,8 +45,9 @@ def handle_calculate_IK(req):
                         [ sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
                         [                 0,                 0,           0,             1]])
             return TF
-
-        ## Substitute DH_Table
+        
+        # Create individual transformation matrices
+        # Substitute DH_Table
         T0_1 = TF_Mat(alpha0, a0, d1, q1).subs(dh)
         T1_2 = TF_Mat(alpha1, a1, d2, q2).subs(dh)
         T2_3 = TF_Mat(alpha2, a2, d3, q3).subs(dh)
@@ -70,6 +72,30 @@ def handle_calculate_IK(req):
         #T_total = (T0_7 * R_corr)
         ###########################################################
         
+        # Find EE rotation matrix RPY (Roll, Pitch, Yaw)
+        r,p,y = symbols('r p y')
+
+        # Roll
+        ROT_x = Matrix([[       1,       0,       0],
+                        [       0,  cos(r), -sin(r)],
+                        [       0,  sin(r),  cos(r)]])
+        # Pitch
+        ROT_y = Matrix([[  cos(p),       0,  sin(p)],
+                        [       0,       1,       0],
+                        [ -sin(p),       0,  cos(p)]])
+        # Yaw
+        ROT_z = Matrix([[  cos(y), -sin(y),       0],
+                        [  sin(y),  cos(y),       0],
+                        [       0,       0,       1]])
+
+        ROT_EE = ROT_z * ROT_y * ROT_x
+        
+        # Compensate for rotation discrepancy between DH parameters and Gazebo
+        # Correction Needed to Account for Orientation Difference Between
+        # Definition of Gripper Link_G in URDF versus DH Convention
+        ROT_corr = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
+        ROT_EE = ROT_EE * ROT_corr     
+        
         # Initialize service response
         joint_trajectory_list = []
         for x in xrange(0, len(req.poses)):
@@ -88,42 +114,19 @@ def handle_calculate_IK(req):
                         [py],
                         [pz]])
             
-            # Requested end-effector (EE) orientation
+            # Extract end-effector position and orientation from request
             (roll,pitch,yaw) = tf.transformations.euler_from_quaternion(
                 [req.poses[x].orientation.x,
                 req.poses[x].orientation.y,
                 req.poses[x].orientation.z,
                 req.poses[x].orientation.w])
-
-            # Find EE rotation matrix RPY (Roll, Pitch, Yaw)
-            r,p,y = symbols('r p y')
-
-            # Roll
-            ROT_x = Matrix([[       1,       0,       0],
-                            [       0,  cos(r), -sin(r)],
-                            [       0,  sin(r),  cos(r)]])
-            # Pitch
-            ROT_y = Matrix([[  cos(p),       0,  sin(p)],
-                            [       0,       1,       0],
-                            [ -sin(p),       0,  cos(p)]])
-            # Yaw
-            ROT_z = Matrix([[  cos(y), -sin(y),       0],
-                            [  sin(y),  cos(y),       0],
-                            [       0,       0,       1]])
-
-            ROT_EE = ROT_z * ROT_y * ROT_x
-
-            # Correction Needed to Account for Orientation Difference Between
-            # Definition of Gripper Link_G in URDF versus DH Convention
-
-            ROT_corr = ROT_z.subs(y, radians(180)) * ROT_y.subs(p, radians(-90))
-            ROT_EE = ROT_EE * ROT_corr
+         
             ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
 
             # Calculate Wrest Center
             WC = EE - (0.303) * ROT_EE[:,2]
 
-            # Calculate joint angles
+            # Calculate joint angles using Geometric IK method
             # Calculate theat1
             theta1 = atan2(WC[1],WC[0])
 
@@ -139,10 +142,12 @@ def handle_calculate_IK(req):
             theta2 = pi/2 - a - atan2(WC[2]-0.75, sqrt(WC[0]*WC[0]+WC[1]*WC[1])-0.35)
             theta3 = pi/2 - (b+0.036) # 0.036 accounts for sag in link4 of -0.054m
 
-            # Extract rotation matrix R0_3 from transformation matrix T0_3 the substiute angles q1-3
+            # Extract rotation matrices from the transformation matrices
+            # Extract rotation matrix R0_3 from transformation matrix T0_3 then substitute angles q1-3
             R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
             R0_3 = R0_3.evalf(subs={q1: theta1, q2: theta2, q3:theta3})
-
+            
+            
             # Get rotation matrix R3_6 from (inverse of R0_3 * R_EE)
             R3_6 = R0_3.inv(method="LU") * ROT_EE
 
